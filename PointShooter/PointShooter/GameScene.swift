3c3d21c90078479aff028291b366bc86e6b2902f
Ashton Wai <indexsky@users.noreply.github.com>
2016-03-01 01:40:40 -0500

    Add spawn single enemy function, bullet vs enemy collision detection, and utility functions.

--- PointShooter/PointShooter/GameScene.swift
+++ PointShooter/PointShooter/GameScene.swift
@@ -6,9 +6,16 @@
 //  Copyright (c) 2016 Ashton Wai & Zachary Bebel. All rights reserved.
 //
 
+struct PhysicsCategory {
+    static let None      : UInt32 = 0
+    static let All       : UInt32 = UInt32.max
+    static let Enemy     : UInt32 = 0b1
+    static let Bullet    : UInt32 = 0b10
+}
+
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     var ship:SKSpriteNode!
     var lastTime:CFTimeInterval = 0
     var deltaTime:CFTimeInterval = 0
@@ -17,11 +24,35 @@ class GameScene: SKScene {
     var fireTimer:Float = 0.0
     var autoFiring = false
     
+    let playableRect: CGRect
+    
+    override init(size: CGSize) {
+        // make constant for max aspect ratio support 16:9
+        let maxAspectRatio: CGFloat = 16.0 / 9.0
+        // calculate playable height
+        let playableHeight = size.width / maxAspectRatio
+        // center playable rectangle on the screen
+        let playableMargin = (size.height - playableHeight) / 2.0
+        // make centered rectangle on the screen
+        playableRect = CGRect(x: 0, y: playableMargin, width: size.width, height: playableHeight)
+        
+        // call the initializer of the superclass
+        super.init(size: size)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        // must also override required NSCoder initializer when override the default initializer
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        physicsWorld.gravity = CGVectorMake(0, 0)
+        physicsWorld.contactDelegate = self
+        
         ship = SKSpriteNode(imageNamed:"Spaceship")
-        ship.xScale = 0.25
-        ship.yScale = 0.25
+        ship.xScale = 0.5
+        ship.yScale = 0.5
         ship.position = CGPointMake(size.width/2, size.height/2)
         self.addChild(ship)
         
@@ -29,6 +60,23 @@ class GameScene: SKScene {
         self.view!.addGestureRecognizer(gestureRecognizer)
         
         fireTimer = fireRate
+        
+        spawnEnemy()
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+        deltaTime = currentTime - lastTime
+        lastTime = currentTime
+        
+        if (autoFiring) {
+            if (fireTimer > 0) {
+                fireTimer -= Float(deltaTime)
+            } else {
+                autoFire()
+                fireTimer = fireRate
+            }
+        }
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -59,29 +107,71 @@ class GameScene: SKScene {
         }
     }
     
+    func didBeginContact(contact: SKPhysicsContact) {
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        guard firstBody.node != nil else {
+            return
+        }
+        guard secondBody.node != nil else {
+            return
+        }
+        
+        if ((firstBody.categoryBitMask & PhysicsCategory.Enemy != 0) &&
+            (secondBody.categoryBitMask & PhysicsCategory.Bullet != 0)) {
+                bulletDidCollideWithEnemy(firstBody.node as! SKShapeNode, enemy: secondBody.node as! SKShapeNode)
+        }
+    }
+    
+    func bulletDidCollideWithEnemy(bullet:SKShapeNode, enemy:SKShapeNode) {
+        bullet.removeFromParent()
+        enemy.removeFromParent()
+        
+        spawnEnemy()
+    }
+    
     func autoFire() {
         let circle = SKShapeNode.init(circleOfRadius: 10)
         circle.fillColor = SKColor.redColor()
         circle.position = CGPointMake(ship.position.x, ship.position.y)
-        let moveAction = SKAction.moveBy(CGVector(dx: cos(ship.zRotation + CGFloat(M_PI/2))*1000, dy: sin(ship.zRotation + CGFloat(M_PI/2))*1000), duration: bulletSpeed)
+        
+        circle.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+        circle.physicsBody?.dynamic = true
+        circle.physicsBody?.categoryBitMask = PhysicsCategory.Bullet
+        circle.physicsBody?.contactTestBitMask = PhysicsCategory.Enemy
+        circle.physicsBody?.collisionBitMask = PhysicsCategory.None
+        circle.physicsBody?.usesPreciseCollisionDetection = true
+        
+        let moveAction = SKAction.moveBy(CGVector(dx: cos(ship.zRotation + CGFloat(M_PI/2))*2500, dy: sin(ship.zRotation + CGFloat(M_PI/2))*2500), duration: bulletSpeed)
         let deleteAction = SKAction.removeFromParent()
         let blockAction = SKAction.runBlock({print("Done moving, deleting")})
         circle.runAction(SKAction.sequence([moveAction,blockAction,deleteAction]))
         addChild(circle)
     }
     
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-        deltaTime = currentTime - lastTime
-        lastTime = currentTime
+    func spawnEnemy() {
+        let enemy = SKShapeNode(rectOfSize: CGSize(width: 75, height: 75))
+        enemy.fillColor = UIColor.greenColor()
         
-        if (autoFiring) {
-            if (fireTimer > 0) {
-                fireTimer -= Float(deltaTime)
-            } else {
-                autoFire()
-                fireTimer = fireRate
-            }
-        }
+        enemy.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 75, height: 75))
+        enemy.physicsBody?.dynamic = true
+        enemy.physicsBody?.categoryBitMask = PhysicsCategory.Enemy
+        enemy.physicsBody?.contactTestBitMask = PhysicsCategory.Bullet
+        enemy.physicsBody?.collisionBitMask = PhysicsCategory.None
+        
+        enemy.position = CGPoint(
+            x: CGFloat.random(min: CGRectGetMinX(playableRect),
+                max: CGRectGetMaxX(playableRect)),
+            y: CGFloat.random(min: CGRectGetMinY(playableRect),
+                max: CGRectGetMaxY(playableRect)))
+        addChild(enemy)
     }
 }
